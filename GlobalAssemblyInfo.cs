ad98d027098e74987babbcd33a1cf419cadd174f
Justin Dearing <zippy1981@gmail.com>
2013-10-28 20:23:02 -0400

    Added GlobalAssemblyInfo.cs

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Ki")]
+[assembly: AssemblyProduct("Confuser")]
+[assembly: AssemblyCopyright("Make by Ki")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
